ffe9de1ae416b275157463c0a695bf1229e46baa
siba014 <sprasad@inspirata.com>
2024-07-17 12:53:11 +0530

    Max sum of subarrays - Sliding Window

---
+++ sliding-window-subarray-max-sum.js
@@ -0,0 +1,24 @@
+let arr = [2,3,5,2,9,7,1];
+let window = 3;
+
+function maxSubArraySum(arr, window) {
+    let maxSum = 0;
+    let sum = 0;
+    let dequeue = []
+    for(let i=0;i<arr.length;i++){
+        if(dequeue.length < window){
+            sum+=arr[i]
+            dequeue.push(arr[i]);
+        }
+        else{
+            sum > maxSum ? maxSum = sum : null;
+            sum= sum - dequeue[0];
+            dequeue.shift()
+            dequeue.push(arr[i])
+            sum= sum + arr[i]
+        }
+    }
+    return maxSum;
+}
+
+console.log(maxSubArraySum(arr,window));
